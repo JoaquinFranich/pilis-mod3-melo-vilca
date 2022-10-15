16d40f2c58b87db7d4fa345b9d9bc627ae3232b8
Joaquin Franich <joaquinmelo90@gmail.com>
2022-10-15 01:07:32 -0300

    Cambios en Card.css.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -13,13 +13,18 @@ const Card = ({card}) =>{
 
     return(
         <div className="card-container">
-            <p>ID: {id}</p>
-            <p>Nombre: {name}</p>
-            <p>Latitud: {latitude}</p>
-            <p>Longitud: {longitude}</p>
-            <p>Temperatura: {temperature} °C</p>
-            <p>Viento: {windspeed} m/s</p>
-            <button onClick={deleteCard}>eliminar</button>
+            <div className="img-container">
+                <img src="https://picsum.photos/seed/picsum/200" alt="" />
+            </div>
+            <div className="card-content">
+                <p className="id">ID: {id}</p>
+                <p className="name">Nombre: {name}</p>
+                <p className="lat">Latitud:     {latitude}</p>
+                <p className="lon">Longitud:    {longitude}</p>
+                <p className="temp">Temperatura:    {temperature} °C</p>
+                <p className="wind">Viento:     {windspeed} m/s</p>
+            </div>
+            <button className="btn-delete" onClick={deleteCard}>eliminar</button>
         </div>
     )
 }
